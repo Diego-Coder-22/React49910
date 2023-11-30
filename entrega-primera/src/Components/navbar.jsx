ce296439f29051bd4b9a46018a6f93f8a30114c2
Diego Fernandez <diego.fnicolini@gmail.com>
2023-11-30 22:13:58 +0100

    first commit

---
+++ entrega-primera/src/Components/navbar.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+const Navbar = () => {
+    return (
+        <header>
+            <div>
+                <h1>LOGO</h1>
+
+                <nav>
+                    <a href="#">Enlace 1</a>
+                    <a href="#">Enlace 2</a>
+                    <a href="#">Enlace 3</a>
+                </nav>
+            </div>
+        
+        </header>
+    )
+}
+
+export default Navbar
